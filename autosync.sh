57b145d19ffc1a2cfc3db733a6c88f377380c767
Greg Mann <gregorywmann@gmail.com>
2016-03-14 14:45:12 -0700

    Added ssh_key and machine_id arguments.

--- autosync.sh
+++ autosync.sh
@@ -4,61 +4,60 @@
 #
 # Dependency: fswatch (https://github.com/emcrisostomo/fswatch)
 #
-# Assumptions:
-#   * We assume that the platform has a username defined in the `case`
-#     statement below.
-#
-# Usage: ./autosync.sh [platform] [host IP]
 ################################################################################
 
-DEFAULT_PLATFORM=centos
 AUTOSYNC_DIR=$HOME/.autosync
 #REMOTE_DEV_DIR="\\\$HOME/src"
 REMOTE_DEV_DIR="/mnt/src"
 SSH_KEY=$HOME/.ssh/greg.pem
+
+# Note: all arguments except `machine_id` and `host_ip` are required. On the
+#       first run of this script, `host_ip` must be provided.
+USAGE="Usage: ./autosync.sh [remote_username] \
+[ssh_key_file] [host_ip] [machine_id]"
 
 # Create the log directory if necessary.
 if [ ! -e "$AUTOSYNC_DIR" ]; then
   mkdir -p $AUTOSYNC_DIR
 fi
 
-# Populate the platform name.
 if [ -z "$1" ]; then
-  platform=$DEFAULT_PLATFORM
+  echo "ERROR: No arguments were provided"
+  echo $USAGE
+  exit 1
 else
-  platform=$1
+  remote_username=$1
 fi
 
-# Set the remote username.
-case $platform in
-centos)
-  remote_username=centos
-  ;;
-ubuntu)
-  remote_username=ubuntu
-  ;;
-*)
-  echo "You specified an undefined platform."
+if [ -z "$2" ]; then
+  echo "ERROR: SSH key file was not provided"
   exit 1
-  ;;
-esac
+else
+  ssh_key=$2
+fi
 
-config_file=$AUTOSYNC_DIR/${platform}.conf
+if [ -z "$4" ]; then
+  machine_id="default"
+else
+  machine_id=$4
+fi
+
+config_file=$AUTOSYNC_DIR/${machine_id}.conf
 
 # Populate the host IP.
-if [ -z "$2" ]; then
+if [ -z "$3" ]; then
   # Make sure the IP has already been stored.
   if [ ! -e $config_file ] || [ ! -s $config_file ]; then
     echo "If no IP is provided, it must be specified in $config_file."
     exit 1
   fi
 
-  host_ip=`grep "${platform}_ip" $config_file | awk '{print $2}'`
+  host_ip=`grep "${machine_id}_ip" $config_file | awk '{print $2}'`
 else
-  host_ip=$2
+  host_ip=$3
 
-  # Store this IP for later use.
-  echo "${platform}_ip $2" > $config_file
+  # Store this IP for later use, associated with the current `machine_id`.
+  echo "${machine_id}_ip $host_ip" > $config_file
 fi
 
 # The current directory name, without the full path.
@@ -70,7 +69,7 @@ target_dir=$DEV_DIR/$current_dir
 rsync_command="rsync -avz --rsync-path=\"sudo mkdir -p                         \
   $REMOTE_DEV_DIR/$current_dir && sudo chown -R                                \
   $remote_username:$remote_username $REMOTE_DEV_DIR && rsync\" --delete        \
-  --exclude=\".git/*\" --exclude=\"build/*\" -e \"ssh -i $SSH_KEY -l           \
+  --exclude=\".git/*\" --exclude=\"build/*\" -e \"ssh -i $ssh_key -l           \
   $remote_username\" . $remote_username@$host_ip:$REMOTE_DEV_DIR/$current_dir"
 
 # Perform an initial sync.
@@ -78,5 +77,5 @@ eval $rsync_command
 
 # Start up the background process.
 eval "fswatch -0 -o -e .git/ . | xargs -0 -I {} $rsync_command >               \
-  $AUTOSYNC_DIR/${platform}.log 2>&1 &"
+  $AUTOSYNC_DIR/${machine_id}.log 2>&1 &"
 
